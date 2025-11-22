b51560d09efbfb55c3e0a9a13fd41a6b0ea66998
agb727387 <107209012+agb727387@users.noreply.github.com>
2025-11-22 13:24:46 -0500

    Update page title and script logic

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('testing');
+/*
+
+1 = rock
+2 = paper
+3 = scissors
+
+*/
+
+let playerChoice = prompt('Make a choice:\n\nPress 1 for rock\nPress 2 for paper\nPress 3 for scissors');
+let computer;
+
+let randomNumber
+
+console.log(Math.floor(Math.random() * 3) + 1);
+function getComputerChoice(choice) {
+    choice = Math.random();
+    return Math.random();
+}
+
+function getHumanChoice() {
+
+}
